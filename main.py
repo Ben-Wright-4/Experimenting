3d112f6a0ef609fcdad1aa63d36d4eb019987dbd
Ben-Wright-4 <benwright2302@gmail.com>
2025-02-10 15:34:30 +0000

    .

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 import sqlite3 as sql
+from flask import render_template
 
 teams = ["Team1", "Team2","Team3", "Team4","Team5", "Team6","Team7", "Team8", "Team9", "Team10","Team11", "Team12","Team13", "Team14","Team15", "Team16"]
 def createTable():
